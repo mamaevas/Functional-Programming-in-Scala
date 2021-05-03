a153b5b5e74a2151062dd6e1e796a1e65c3c6f24
mamaevas <alexander.mamaev.ru@gmail.com>
2021-05-03 13:31:35 +0500

    ex 3.13

--- src/main/scala/functional/programming/in/scala/exercise/Exercise3.scala
+++ src/main/scala/functional/programming/in/scala/exercise/Exercise3.scala
@@ -126,4 +126,13 @@ object List {
         foldLeft(list, List[A]())((acc, next) => Cons(next, acc))
     }
 
+    /**
+     * Exercise 3.13
+     * Implementing foldRight via foldLeft is useful because it lets us implement
+     * foldRight tail-recursively, which means it works even for large lists without overflowing the stack
+     */
+    def foldRightViaLeft[A, B](list: List[A], z: B)(f: (A, B) => B): B = {
+        foldLeft(reverse(list), z)((acc, next) => f(next, acc))
+    }
+
 }
--- src/test/scala/functional/programming/in/scala/exercise/Exercise3Test.scala
+++ src/test/scala/functional/programming/in/scala/exercise/Exercise3Test.scala
@@ -7,6 +7,18 @@ class Exercise3Test extends AnyFlatSpec with Matchers {
 
     import List._
 
+    "foldRight" should "be fine" in {
+        val testList = List(1, 2, 3)
+        val res = foldRight(testList, -4)((next, acc) => acc - next) // -4 - 3 - 2 - 1 = -10
+        res shouldBe -10
+    }
+
+    "foldLeft" should "be fine" in {
+        val testList = List(1, 2, 3)
+        val res = foldLeft(testList, -4)((acc, next) => acc - next) // -4 - 1 - 2 - 3 = -10
+        res shouldBe -10
+    }
+
     "Exercise 3.1" should
         "What will be the result of the following match expression?" in {
         val res3_1 = List(1, 2, 3, 4, 5) match {
@@ -21,23 +33,17 @@ class Exercise3Test extends AnyFlatSpec with Matchers {
 
     "Exercise 3.2" should
         "Implement the function tail for removing the first element of a List" in {
-        val testList = List(1, 2, 3)
-        val res3_2 = tail(testList)
-        res3_2 shouldBe Cons(2, Cons(3, Nil))
+        tail(List(1, 2, 3)) shouldBe Cons(2, Cons(3, Nil))
     }
 
     "Exercise 3.3" should
         "implement the function setHead for replacing the first element of a List with a different value" in {
-        val testList = List(1, 2, 3)
-        val res3_3 = setHead(testList, 25)
-        res3_3 shouldBe Cons(25, Cons(2, Cons(3, Nil)))
+        setHead(List(1, 2, 3), 25) shouldBe Cons(25, Cons(2, Cons(3, Nil)))
     }
 
     "Exercise 3.4" should
         "Generalize tail to the function drop, which removes the first n elements from a list" in {
-        val testList = List(1, 2, 3)
-        val res3_4 = drop(testList, 2)
-        res3_4 shouldBe Cons(3, Nil)
+        drop(List(1, 2, 3), 2) shouldBe Cons(3, Nil)
     }
 
     "Exercise 3.5" should
@@ -54,40 +60,33 @@ class Exercise3Test extends AnyFlatSpec with Matchers {
         res3_5 shouldBe Cons(2, Cons(3, Nil))
     }
 
-    "Exercise 3.6" should
-        "returns a List consisting of all but the last element of a List" in {
-        val testList = List(1, 2, 3)
-        val res3_6 = init(testList)
-        res3_6 shouldBe Cons(1, Cons(2, Nil))
+    "Exercise 3.6" should "returns a List consisting of all but the last element of a List" in {
+        init(List(1, 2, 3)) shouldBe Cons(1, Cons(2, Nil))
     }
 
-    "Exercise 3.9" should
-        "Compute the length of a list using foldRight" in {
-        val res3_9 = lengthRight(List(1, 2, 3, 4))
-        res3_9 shouldBe 4
+    "Exercise 3.9" should "Compute the length of a list using foldRight" in {
+        lengthRight(List(1, 2, 3, 4)) shouldBe 4
     }
 
     "Exercise 3.10" should "be fine" in {
-        val res3_10 = foldLeft(List(1, 2, 3), 4)(_ + _)
-        res3_10 shouldBe 10
+        foldLeft(List(1, 2, 3), 4)(_ + _) shouldBe 10
     }
 
     "Exercise 3.11" should
         "Write sum, product, and a function to compute the length of a list using foldLeft." in {
-        val res3_11_1 = sumF(List(1, 2, 3, 4))
-        res3_11_1 shouldBe 10
-
-        val res3_11_2 = productF(List(1, 2, 3, 4))
-        res3_11_2 shouldBe 24
-
-        val res3_11_3 = lengthLeft(List(1, 2, 3, 4))
-        res3_11_3 shouldBe 4
+        sumF(List(1, 2, 3, 4)) shouldBe 10
+        productF(List(1, 2, 3, 4)) shouldBe 24
+        lengthLeft(List(1, 2, 3, 4)) shouldBe 4
     }
 
     "Exercise 3.12" should "" +
         "Write a function that returns the reverse of a list (given List(1,2,3) it returns List(3,2,1))." +
         "See if you can write it using a fold" in {
-        val res3_12_1 = reverse(List(1, 2, 3, 4))
-        res3_12_1 shouldBe List(4, 3, 2, 1)
+        reverse(List(1, 2, 3, 4)) shouldBe List(4, 3, 2, 1)
+    }
+
+    "Exercise 3.13" should "Implementing foldRight via foldLeft" in {
+        foldRightViaLeft(List(1, 2, 3), -4)((next, acc) => acc - next) shouldBe -10
+        foldRightViaLeft(List(1, 2, 3), -4)(_ - _) shouldBe 6
     }
 }
